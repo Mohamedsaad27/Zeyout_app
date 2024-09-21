906b5fafcd1ce1048461ec2fd691ed75179ada42
Mohamed Saad <sa3doni2714@gmail.com>
2024-09-21 18:57:33 +0300

    -AddCustomValidationRequests -AddMiddlewareToHandleLocalizationInApplication

---
+++ app/Http/Requests/Brand/StoreBrandRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests\Brand;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreBrandRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    
+    public function rules(): array
+    {
+        return [
+            'name_ar' => 'required|string|max:255',
+            'name_en' => 'required|string|max:255',
+            'logo' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ];
+    }
+}
