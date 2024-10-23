9a7e1c99598882281199a79ce15ad8c8a7bb41bf
Mohamed Saad <sa3doni2714@gmail.com>
2024-10-23 10:28:10 +0300

    FixResponseInBrands

--- app/Http/Resources/SingleBrandResource.php
+++ app/Http/Resources/SingleBrandResource.php
@@ -15,6 +15,7 @@ class BrandResource extends JsonResource
             'id' => $this->id,
             'name' => $locale == 'ar' ? $this->name_ar : $this->name_en,
             'logo' => $this->logo,
+            'products' => ProductResource::collection($this->products),
         ];
     }
 }
