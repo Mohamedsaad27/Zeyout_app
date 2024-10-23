8cfc32fc781d7696f75563d230e7d9f24f265180
Mohamed Saad <sa3doni2714@gmail.com>
2024-10-23 11:06:31 +0300

    FixResponseInBrands

--- app/Http/Resources/SingleProductResource.php
+++ app/Http/Resources/SingleProductResource.php
@@ -22,7 +22,7 @@ class SingleProductResource extends JsonResource
             'image' => $this->image,
             'is_favorite' => $this->favorites()->where('user_id', auth()->id())->exists(),
             'product_variants' => ProductVariantResource::collection($this->product_variants),
-            'categories' => $this->category ? ($locale == 'ar' ? $this->category->pluck('name_ar') : $this->category->pluck('name_en')) : [],
+            'categories' => $this->categories ? ($locale == 'ar' ? $this->categories->pluck('name_ar') : $this->categories->pluck('name_en')) : [],
             'brand' => $this->brand ? ($locale == 'ar' ? $this->brand->name_ar : $this->brand->name_en) : null,
             'API' => $this->API,
             'related_products' => ProductResource::collection($this->relatedProducts),
